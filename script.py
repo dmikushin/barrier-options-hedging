3322ae6eb2de29470f9170e1cdfedf36c0169a0e
Dmitry Mikushin <dmitry@kernelgen.org>
2020-05-30 20:33:39 +0200

    Starting script refactoring

--- script.py
+++ script.py
@@ -1,8 +1,20 @@
 import numpy as np
-x = np.linspace(0, 2.0 * np.pi, 100)
-y = np.sin(x)
 
-from matplotlib import pyplot as plt
-plt.figure()
-plt.plot(x, y)
-plt.show()
+#####################################################################
+#               Generate random paths for shares
+#####################################################################
+
+nPaths = 10 # number of paths
+nSteps = 252 # number of days
+
+# set the random seed https://en.wikipedia.org/wiki/Random_seed
+np.random.seed(123)
+
+# random generation for the normal distribution with mean 0 and standard deviation 1
+dw = np.random.normal(loc=0.0, scale=1.0, size=nPaths*nSteps)
+
+# reshape normal distribution to 2D
+dw = dw.reshape((nPaths, nSteps))
+
+Spot = 13310
+vola = 0.2
